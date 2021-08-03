8e2eae4f60ed3ab2f3490ba61e33eb9c05616b9c
DeepakNair908 <88398221+DeepakNair908@users.noreply.github.com>
2021-08-03 23:12:57 +0530

    Add files via upload

---
+++ movies.py
@@ -0,0 +1,42 @@
+import sqlite3
+conn=sqlite3.connect('MOVIES.db')
+c=conn.cursor()
+
+##### THE TABLE CREATION ########
+
+
+
+#c.execute("""CREATE TABLE MOVIES (
+ #           NAME text,
+  #          ACTOR text,
+   #         YEAR integer,
+    #        DIRECTOR NAME text
+     #      )""")
+
+     
+##### INSERTION INTO TABLE ######
+
+
+     
+#c.execute("INSERT INTO MOVIES VALUES('PULP FICTION','John Travolta',1994,'Quentin Tarantino')")
+#c.execute("INSERT INTO MOVIES VALUES('Fight Club','Brad Pitt',1999,'David Fincher')")
+#c.execute("INSERT INTO MOVIES VALUES('The Dark Knight','Christian Bale',2008,'Christopher Nolan')")
+
+
+##### EXECUTING COMMANDS #######
+
+     
+c.execute("select * from MOVIES ")
+
+print(c.fetchall())
+
+c.execute("select * from MOVIES WHERE  ACTOR = 'Brad Pitt'")
+
+print(c.fetchall())
+
+conn.commit()
+
+conn.close()
+
+#### To be honest i was in a hurry as my university exams were going on
+#### and it will all be done in a week so i preffered python over C and Java
